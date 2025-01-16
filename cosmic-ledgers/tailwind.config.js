7734f29a064767c3afb502d0c14773d0712835f5
hayamwuruk <rafibuss4@gmail.com>
2025-01-16 09:44:53 +0700

    setup tailwind

--- cosmic-ledgers/tailwind.config.js
+++ cosmic-ledgers/tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    './components/**/*.{js,jsx}',
+    './pages/**/*.{js,jsx}',
+  ],
   theme: {
     extend: {},
   },
--- cosmic-ledgers/vite.config.js
+++ cosmic-ledgers/vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
+import path from "path"
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      process: "process/browser",
+      buffer: "buffer",
+      util: "util",
+    },
+  },
   server: {
     port: 3001, // Change this number to any port you want
   },
