bfb4d3d74c01d0de2088cfad627ece460c3c4f23
hayamwuruk <rafibuss4@gmail.com>
2025-01-22 20:42:16 +0700

    fixing api key

--- cosmic-ledgers/src/resolvers.js
+++ cosmic-ledgers/src/resolvers.js
@@ -2,6 +2,7 @@
   import { gql } from 'apollo-server-express';
   import axios from 'axios';
   import puppeteer from 'puppeteer';
+  import dotenv from 'dotenv'; // Add this line to import dotenv
 
   const resolvers = {
     Query: {
@@ -149,7 +150,7 @@
         try {
           const response = await fetch('https://prod-kline-rest.supra.com/latest?trading_pair=supra_usdt', {
             headers: {
-              'x-api-key': '356464a14d94ec3c455480727eee9c4fd58233bfd8cdeb1701d2aec132d4d670'
+              'x-api-key': process.env.SUPRA_ORACLE_API_KEY // Use the environment variable here
             }
           });
       
