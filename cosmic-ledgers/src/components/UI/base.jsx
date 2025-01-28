6a7c74916b0651fb039269c21a574d6542a707ca
hayamwuruk <rafibuss4@gmail.com>
2025-01-28 09:18:22 +0700

    fix-child problems

--- cosmic-ledgers/src/components/UI/base.jsx
+++ cosmic-ledgers/src/components/UI/base.jsx
@@ -1,25 +1,25 @@
-import React from "react";
-import WalletSelect from "./walletselect";
-import Content from "./contentchart";
-import Navbar from "./navbar";
-import WalletProvider, { useWalletContext } from "./WalletContext";
+  import React from "react";
+  import WalletSelect from "./walletselect";
+  import Content from "./contentchart";
+  import Navbar from "./navbar";
+  import WalletProvider, { useWalletContext } from "./WalletContext";
 
-const BaseLayer = ({ children }) => {
-  return (
-    <WalletProvider>
-      <div className="flex items-center justify-between">
-        {/* main cards */}
-        <div className="custom-width mx-auto h-full glass px-10 pb-3">
-          <WalletSelect />
-          <Content />
-          {children}  {/* Here's where we add the children */}
-          <div className="w-full justify-items-end mt-2">
-            <img className="h-8 w-auto" src="./src/assets/name_logo.png" />
+  const BaseLayer = ({ children }) => {
+    return (
+
+        <div className="flex items-center justify-between">
+          {/* main cards */}
+          <div className="custom-width mx-auto h-full glass px-10 pb-3">
+            <WalletSelect />
+            <Content />
+            {children}  {/* Here's where we add the children */}
+            <div className="w-full justify-items-end mt-2">
+              <img className="h-8 w-auto" src="./src/assets/name_logo.png" />
+            </div>
           </div>
         </div>
-      </div>
-    </WalletProvider>
-  );
-};
 
-export default BaseLayer;
+    );
+  };
+
+  export default BaseLayer;
